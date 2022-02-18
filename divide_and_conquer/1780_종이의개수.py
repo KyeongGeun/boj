7888dc4c439656d089e1353c6ed3ef38e4007ff0
KKG <kkg05102@gmail.com>
2022-02-18 20:52:26 +0900

    boj

---
+++ divide_and_conquer/1780_종이의개수.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+def slice(x, y, n, li):
+    v = int(li[y][x])+1
+    if n == 1:
+        cnt[v] += 1
+        return
+
+    s = str(v-1)
+    flag = False    
+    
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if li[j][i] != s:
+                flag = True
+            if flag:
+                break
+        if flag:
+            break
+    
+    if flag:
+        for i in range(3):
+            for j in range(3):
+                slice(x + j*n//3, y + i*n//3, n//3, li)
+    else:
+        cnt[v] += 1
+
+n = int(input())
+li = [input().split() for _ in range(n)]
+
+cnt = [0, 0, 0]
+s = slice(0, 0, n, li)
+
+print(*cnt, sep='\n')
