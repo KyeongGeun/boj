2ca5c96d97af2b8b92609ff6ba0d07f3730214db
KKG <kkg05102@gmail.com>
2022-03-03 11:52:36 +0900

    boj

---
+++ DFS_and_BFS/2606_바이러스.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(v):
+    log.add(v)
+
+    li = []
+    for value in edge[v]:
+        if value not in log:
+            li += dfs(value)
+
+    return [v] + li
+
+n = int(input())
+edge = [[] for _ in range(n + 1)]
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    edge[a].append(b)
+    edge[b].append(a)
+
+for i in range(1, len(edge)):
+    edge[i].sort()
+
+log = set()
+
+d = dfs(1)
+print(len(d) - 1)
