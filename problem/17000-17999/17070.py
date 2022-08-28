f9c319a2d4cc293d2d3e798005dc2b80bfd4dc8f
kkg5 <kkg05102@gmail.com>
2022-08-28 10:51:52 +0900

    파이프 옮기기 1

--- problem/17000-17999/17070.py
+++ problem/17000-17999/17070.py
@@ -8,16 +8,19 @@ dp = [[[0 for _ in range(3)] for _ in range(n)] for _ in range(n)]
 
 dp[0][1][0] = 1
 
-for i in range(n):
-    for j in range(n):
+for i in range(2, n):
+    if li[0][i]:
+        break
+    dp[0][i][0] = 1
+
+for i in range(1, n):
+    for j in range(1, n):
         if li[i][j] == 1:
             continue
-        if i != 0:
-            dp[i][j][2] += dp[i - 1][j][1] + dp[i - 1][j][2]
-        if j != 0:
-            dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][1]
+        dp[i][j][2] += dp[i - 1][j][1] + dp[i - 1][j][2]
+        dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][1]
 
-        if i == 0 or j == 0 or li[i - 1][j] == 1 or li[i][j - 1] == 1:
+        if li[i - 1][j] or li[i][j - 1]:
             continue
         dp[i][j][1] += sum(dp[i - 1][j - 1])
 
