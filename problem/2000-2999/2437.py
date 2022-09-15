9e389c3bbc5ebdf3a4a41d34b703fc91bd644bb2
kkg5 <kkg05102@gmail.com>
2022-09-15 14:21:34 +0900

    저울

--- problem/2000-2999/2437.py
+++ problem/2000-2999/2437.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 input()
 li = sorted(list(map(int, input().split())))
 
-max = 0
+max = 1
 for v in li:
-    if v > max + 1:
+    if v > max:
         break
     else:
         max += v
 
-print(max + 1)
+print(max)
