0c1108e63ae83bddf016c821fb1c46678541cdd0
kkg5 <kkg05102@gmail.com>
2022-11-25 18:58:01 +0900

    55. Jump Game

--- leetcode/medium/55_jump_game.py
+++ leetcode/medium/55_jump_game.py
@@ -1,21 +1,32 @@
 class Solution:
-    def recur(self, cur, nums, visited):
-        if cur >= len(nums) - 1:
-            return True
-        visited[cur] = True
-
-        for i in range(min(len(nums) - 1, cur + nums[cur]), cur, -1):
-            if visited[i]:
-                continue
-            if self.recur(i, nums, visited):
-                return True
-
-        return False
-
     def canJump(self, nums: List[int]) -> bool:
-        visited = [False] * len(nums)
+        maxv = 0
 
-        if len(nums) == 1:
-            return True
+        for i in range(len(nums)):
+            if i > maxv:
+                break
+            maxv = max(maxv, i + nums[i])
 
-        return self.recur(0, nums, visited)
+        return maxv >= len(nums) - 1
+
+# class Solution:
+#     def recur(self, cur, nums, visited):
+#         if cur >= len(nums) - 1:
+#             return True
+#         visited[cur] = True
+
+#         for i in range(min(len(nums) - 1, cur + nums[cur]), cur, -1):
+#             if visited[i]:
+#                 continue
+#             if self.recur(i, nums, visited):
+#                 return True
+
+#         return False
+
+#     def canJump(self, nums: List[int]) -> bool:
+#         visited = [False] * len(nums)
+
+#         if len(nums) == 1:
+#             return True
+
+#         return self.recur(0, nums, visited)
